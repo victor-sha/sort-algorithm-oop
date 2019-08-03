a46fdd3ab6233c3c31789c886204a8bfcae9c0ba
victor-sha <kassini_90@mail.ru>
2019-08-03 13:14:32 +0300

    add linkedLIst class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sort } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 
 const numbersCollection = new NumbersCollection([10, 0, -5, 1]);
@@ -13,3 +14,11 @@ const charactersCollection = new CharactersCollection('WWaJfansLQQ');
 const sorterCh = new Sort(charactersCollection);
 sorterCh.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-200);
+linkedList.add(30);
+const linkedListSorter = new Sort(linkedList);
+linkedListSorter.sort();
+linkedList.print();
